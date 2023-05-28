a6494c6a20adc0d527d5ad17a004a71c60ae466f
jdelfino <jdelfino@gmail.com>
2023-05-27 23:27:34 -0400

    try an image

---
+++ snippets/strings_practice.py
@@ -0,0 +1,12 @@
+print('Hello!\tI\'m a\ncomplicated\\interesting\n\”string\”')
+print()
+print("\ta\tb\tc")
+print()
+print("\\\\")
+print()
+print("'")
+print()
+print("\"\"\"")
+print()
+print("C:\nin\the downward spiral")
+
