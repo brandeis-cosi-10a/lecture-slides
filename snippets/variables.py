50135c5f1b1ede32382c16cfc925898d8f7d93f8
jdelfino <jdelfino@gmail.com>
2023-05-28 16:24:36 -0400

    var snippet

---
+++ snippets/variables.py
@@ -0,0 +1,19 @@
+tetons = "mountains"
+acadia = "forests"
+
+print(answer)
+print(tetons)
+
+tetons = "beautiful"
+
+print(tetons)
+print(tetons + acadia)
+print(tetons + " " + acadia)
+
+answer = 42
+acadia = acadia + " are peaceful"
+print(acadia + str(answer))
+
+joshua_tree = str(answer + 7)
+print(tetons + " " + joshua_tree)
+
