3cb892c022a0d6cce99f49e44a92f46cdd5b6406
jdelfino <jdelfino@gmail.com>
2023-05-28 16:41:44 -0400

    spacing

--- snippets/age_calculator.py
+++ snippets/age_calculator.py
@@ -8,7 +8,5 @@ print("Here are some age facts, " + name + ":")
 age = year - birth_year
 
 print("You are ~" + str(age) + " years old.")
-
 print("You were ~" + str(y2k - birth_year) + " years old when Y2K happened.")
-
 print("You are approximately " + str(age * 365 * 24 * 60 * 60) + " seconds old!")
