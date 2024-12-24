737c64155820847b762686ce1a6fd06b8b8e4b7b
Joe Delfino <jdelfino@brandeis.edu>
2024-12-24 12:12:46 -0500

    first pass at grade helper, need summary stats

--- lectures/html/index.js
+++ lectures/html/index.js
@@ -5,16 +5,23 @@ let lectures = [
     "04_functions",
     "05_testing_and_debugging",
     "06_loops",
-    "07_lists"
+    "07_lists",
+    "08_dictionaries_sets",
+    "09_file_io",
+    "10_classes",
+    "11_exceptions",
+    "11_modules",
+    "12_recursion"
 ];
 
 (function() {
     const container = document.getElementById("container");
     for(const name of lectures) {
-        let content = "<div class='row'><div class='col-5'>Lecture: " + name + "</div>" +
-            "<div class='col-2'><a href='" + name + ".html'>HTML</a></div>" +
-            "<div class='col-2'><a href='" + name + ".slides.html'>HTML slides</a></div>" +
-            "<div class='col-3'><a href='https://colab.research.google.com/github/brandeis-jdelfino/cosi-10a/blob/main/lectures/notebooks/" + name + ".ipynb'>Interactive slides on Google Colab</a></div>" +
+        let content = "<div class='row'>" +
+            "<div class='five columns'>Lecture: " + name + "</div>" +
+            "<div class='one columns'><a href='" + name + ".html'>HTML</a></div>" +
+            "<div class='two columns'><a href='" + name + ".slides.html'>HTML slides</a></div>" +
+            "<div class='four columns'><a href='https://colab.research.google.com/github/brandeis-jdelfino/cosi-10a/blob/main/lectures/notebooks/" + name + ".ipynb'>Interactive slides on Google Colab</a></div>" +
             "</div>";
         container.insertAdjacentHTML( 'beforeend', content);
     }
